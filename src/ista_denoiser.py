28bf47e8a2e6d9cae676b1285736c546febbe7f8
linhongqin123 <2920266967@qq.com>
2026-01-29 11:11:02 +0800

    Create ista_denoiser.py

---
+++ src/ista_denoiser.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+ISTA 图像去噪算法实现
+"""
+
+import numpy as np
+
+def ista_denoise(image, lambda_val=0.1, max_iter=100):
+    """
+    ISTA 去噪算法
+    """
+    print("ISTA 去噪算法...")
+    # TODO: 实现 ISTA 算法
+    return image
+
+if __name__ == "__main__":
+    print("ISTA Denoiser Module")
